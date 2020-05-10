634d023be9003525f8bffa99e43003defb7884b7
Tyr Chen <tyr.chen@gmail.com>
2020-05-10 14:49:22 -0700

    init flutter template

---
+++ core/core.dart
@@ -0,0 +1,6 @@
+export 'contracts/contracts.dart';
+export 'error/error.dart';
+export 'network/network.dart';
+export 'usecases/usecases.dart';
+export 'util/util.dart';
+export 'widgets/widgets.dart';
