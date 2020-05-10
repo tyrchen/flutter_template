57fa471677dae1ad33440f4fc0577d68dda679f0
Tyr Chen <tyr.chen@gmail.com>
2020-05-10 16:03:25 -0700

    add skeleton for injection container

---
+++ injection_container.dart
@@ -0,0 +1,17 @@
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // bloc
+
+  // repository
+
+  // data sources
+
+  // use cases
+
+  //! core
+
+  //! external
+}
